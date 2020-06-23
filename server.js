2d093ccf4b5a57e0fcd78dbcb4d7f416ebad8475
Ian Aaberg <ian@Ians-MacBook-Pro.local>
2020-06-23 13:36:39 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
 
+const path = require ('path');
 
 const publicPath = path.join(__dirname, 'public');
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(publicPath));
 
-app.get('/', function(req, res){
-    res.send("hi");
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, ()=>{
+  console.log('server running')
 })
-
-
-const port = process.env.PORT || 5000;
-app.listen(port, ()=>{console.log('Node Server Started.')});
