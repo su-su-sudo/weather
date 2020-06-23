ffbda684512cc258c6a34796e8e65a9bc95488f7
Ian Aaberg <ian@Ians-MacBook-Pro.local>
2020-06-23 13:24:26 -0400

    added build

---
+++ client/build/precache-manifest.f37632f61dc528a537fb0b5553313c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dca8f7c48ecd2c196aee50b31606b9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "655ba54df7fea6a54c83",
+    "url": "/static/js/2.944f2fcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.944f2fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b9194b58511bf15a0b",
+    "url": "/static/js/main.2c0a6389.chunk.js"
+  },
+  {
+    "revision": "141b7d42e3227beb65c8",
+    "url": "/static/js/runtime-main.94f71fc2.js"
+  }
+]);
